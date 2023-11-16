bff9735b58078fbd82cb4c60f217aace3a79b5d7
apian <apian@AntonioBook>
2023-11-16 11:35:38 +0100

    Revert "Commit 2"
    This reverts commit b91431c165c5e8c27b746e0b13d6348b861389cd.

--- src/main/java/biz/capuano/tictactoe/Board.java
+++ src/main/java/biz/capuano/tictactoe/Board.java
@@ -12,7 +12,7 @@ class Board {
     }
 
     public void print() {
-             
+        System.out.println("\n┌───┬───┬───┐");        
         for (int i = 0; i < 3; i++) {
             System.out.print("│ ");
             for (int j = 0; j < 3; j++)
