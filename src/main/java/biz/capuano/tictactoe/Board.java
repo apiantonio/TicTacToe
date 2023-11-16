94571953adb6bd90e40c30c2be5a4df2282b1f44
apian <apian@AntonioBook>
2023-11-16 11:34:57 +0100

    Revert "Added String in Board.java"
    This reverts commit 6532556dcce9595f26f108b9e59c23e8460f5f40.

--- src/main/java/biz/capuano/tictactoe/Board.java
+++ src/main/java/biz/capuano/tictactoe/Board.java
@@ -3,8 +3,7 @@ package biz.capuano.tictactoe;
 class Board {
     public static final int SIZE = 3;
     private final char[][] cells;
-    public String x;
-    
+
     public Board() {
         cells = new char[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++)
